2375f79af0634f21cd6375f53d7ce70ce5c0cc88
frederik <fred@toewe.(none)>
2013-05-20 13:23:22 +0200

    initial commit

---
+++ filter.go
@@ -0,0 +1,93 @@
+package filter
+
+import (
+	"reflect"
+	"errors"
+)
+
+
+func Set(filter interface{}) error {
+	
+	pointerValue := reflect.ValueOf(filter)
+	if pointerValue.Kind() != reflect.Ptr {
+		return errors.New("filter must be a pointer")
+	}
+	
+	filterValue := pointerValue.Elem()
+	if filterValue.Kind() != reflect.Func {
+		return errors.New("filter must be a pointer to a function")
+	}
+	
+	filterType := filterValue.Type()
+	
+	if filterType.NumIn() != 2 {
+		return errors.New("filter must have exactly two input parameters")
+	}
+	
+	if filterType.NumOut() != 1 {
+		return errors.New("filter must have exactly one output parameter")
+	}	
+	
+	outputType := filterType.Out(0)
+	if outputType.Kind() != reflect.Slice {
+		return errors.New("filter must return a slice")
+	}
+	
+	arrayType := filterType.In(0)
+	if arrayType.Kind() != reflect.Slice {
+		return errors.New("first argument of filter must be a slice")
+	}
+	
+	predicateType := filterType.In(1)
+	if predicateType.Kind() != reflect.Func {
+		return errors.New("second argument of filter must be a function")
+	}
+	
+	if predicateType.NumIn() != 1 {
+		return errors.New("predicate must have exactly one input parameter")
+	}
+	
+	if predicateType.NumOut() != 1 {
+		return errors.New("predicate must have exactly one output parameter")
+	}
+	
+	if predicateType.Out(0).Kind() != reflect.Bool {
+		return errors.New("predicate must return a Boolean value")
+	}
+
+	inputArrayElType := arrayType.Elem()
+	inputPredicateArgType := predicateType.In(0)
+	outputArrayElType := outputType.Elem()
+	
+	if(inputArrayElType.ConvertibleTo(inputPredicateArgType) == false) {
+		return errors.New("input array elements cannot be converted to predicate parameter")
+	}
+	
+	if(inputArrayElType.ConvertibleTo(outputArrayElType) == false) {
+		return errors.New("input array elemenents cannot be converted to output array elements")
+	}
+	
+	
+	filterImpl := func(in []reflect.Value) []reflect.Value {
+		a := in[0]
+		predicate := in[1]
+		
+		result := reflect.MakeSlice(outputType, 0, 0)
+		
+		for i := 0; i < a.Len(); i++ {
+			el := a.Index(i)
+			predicateArg := []reflect.Value{el.Convert(inputPredicateArgType)}
+			predicateResult := predicate.Call(predicateArg)
+			if predicateResult[0].Bool() {
+				result = reflect.Append(result, el.Convert(outputArrayElType))
+			}
+		}
+
+		return []reflect.Value{result}
+	}
+	
+	function := reflect.MakeFunc(filterType, filterImpl)
+	filterValue.Set(function)
+	
+	return nil;
+}
---
+++ filter_test.go
@@ -0,0 +1,63 @@
+package filter
+
+import (
+	"testing"
+)
+
+func isEven(i int) bool {
+	return i % 2 == 0
+}
+
+func isGreaterThan5(f float32) bool {
+	return f > 5
+}
+
+func TestSetIntFilter(t *testing.T) {
+	
+	var isEvenTest = struct {
+		in  []int
+		out []int
+	}{ []int{1, 2, 3, 4, 5}, []int{2, 4} }
+	
+	var intFilter func([]int, func(int) bool) []int
+	err := Set(&intFilter)
+  if err != nil {
+		t.Fatalf("filter.Set: %v", err)
+	}
+	
+	got := intFilter(isEvenTest.in, isEven)
+	if len(got) != len(isEvenTest.out) {
+		t.Fatalf("expected %#v, got %#v\n", isEvenTest.out, got)
+	}
+	for i, _ := range isEvenTest.out {
+		if got[i] != isEvenTest.out[i] {
+			t.Fatalf("expected %#v, got %#v\n", isEvenTest.out, got)
+		}
+	}
+	
+}
+
+func TestSetFloat32Filter(t *testing.T) {
+	
+	var isGreaterThan5Test = struct {
+		in  []float32
+		out []float32
+	}{ []float32{4, 4.5, 5, 5.5, 6, 6.5, 7}, []float32{5.5, 6, 6.5, 7} 	}
+	
+	var float32Filter func([]float32, func(float32) bool) []float32
+	err := Set(&float32Filter)
+  if err != nil {
+		t.Fatalf("filter.Set: %v", err)
+	}
+	
+	got := float32Filter(isGreaterThan5Test.in, isGreaterThan5)
+	if len(got) != len(isGreaterThan5Test.out) {
+		t.Fatalf("expected %#v, got %#v\n", isGreaterThan5Test.out, got)
+	}
+	for i, _ := range isGreaterThan5Test.out {
+		if got[i] != isGreaterThan5Test.out[i] {
+			t.Fatalf("expected %#v, got %#v\n", isGreaterThan5Test.out, got)
+		}
+	}
+	
+}
